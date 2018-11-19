b82fdba7ad0138606d696235cd2f408d6536c771
MinatoUnder <44467517+MinatoUnder@users.noreply.github.com>
2018-11-19 17:19:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -178,9 +178,60 @@ client.on("message", (message) => {
 });
 
 
+
+client.on('message',async message => {
+
+  if(message.author.bot || message.channel.type === 'dm') return;
+
+  let args = message.content.split(' ');
+
+  if(args[0] === `${prefix}bc`) {
+
+    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
+
+    if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
+
+ 
+
+    let msgCount = 0;
+
+    let errorCount = 0;
+
+    let successCount = 0;
+
+    message.channel.send(`**- [ 🔖 :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ 📥 :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ 📤 :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {
+
+      message.guild.members.forEach(g => {
+
+        g.send(args.slice(1).join(' ')).then(() => {
+
+          successCount++;
+
+          msgCount++;
+
+          msg.edit(`**- [ 🔖 :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ 📥 :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ 📤 :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+
+        }).catch(e => {
+
+          errorCount++;
+
+          msgCount++;
+
+          msg.edit(`**- [ 🔖 :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ 📥 :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ 📤 :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+
+        });
+
+      });
+
+    });
+
+  }
+
+});
+
   client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "bc")) {
+            if (message.content.startsWith(prefix + "bcs")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 

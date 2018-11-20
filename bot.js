f00d89e197b6476e96f49d5974d6f83cf2351484
MinatoUnder <44467517+MinatoUnder@users.noreply.github.com>
2018-11-20 23:24:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,20 +60,6 @@ client.on('message', message=> {
 
 
 
-console.log("Welcome Again !");
- 
-
-          const devs = ['415595760990552065' , '326466810150912000' , '466279297745813507' , '314845344313901057'];
-          const adminprefix = "-";
-          client.on('message', message => {
-              var argresult = message.content.split(`).slice(1).join(' ');
-                if (!devs.includes(message.author.id)) return;
-
-            if (message.content.startsWith(adminprefix + 'st')) {
-              client.user.setGame(argresult, "https://www.twitch.tv/Ninja");
-                message.channel.sendMessage(:white_check_mark:   ${argresult}`)
-      })
-      })
 
 
 
